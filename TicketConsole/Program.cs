f24d7a0dbc17a2e34794e321ccf705db8ceba630
David Marquardt <davemarquardt@gmail.com>
2020-01-29 10:16:52 -0600

    Finished for submission

--- TicketConsole/Program.cs
+++ TicketConsole/Program.cs
@@ -6,8 +6,6 @@
     Tickets.csv
     TicketID, Summary, Status, Priority, Submitter, Assigned, Watching
     1,This is a bug ticket,Open,High,Drew Kjell,Jane Doe,Drew Kjell|John Smith|Bill Jones
-    
-    StreamWriter append = true
 */
 
 using System;
@@ -27,81 +25,102 @@ namespace TicketConsole
 
     class Driver
     {
-        private UserInput input = new UserInput();
-        private List<Ticket> tickets = new List<Ticket>();
-        private FileReader fr = new FileReader();
+        private UserInput _input;
+        private List<Ticket> _tickets;
+        private FileOperations _fo;
+
+        public Driver()
+        {
+            _input = new UserInput();
+            _tickets = new List<Ticket>();
+            _fo = new FileOperations();
+        }
 
         public void Run()
         {
             // open file and create existing tickets
-            List<string[]> ticketArrays = fr.ReadTickets();
+            List<string[]> ticketArrays = _fo.ReadTickets();
 
-            foreach (var t in ticketArrays)
+            foreach (var ticket in ticketArrays)
             {
-                tickets.Add(new Ticket(t[0], t[1], t[2], t[3], t[4], t[5]));
+                _tickets.Add(new Ticket(ticket[0], ticket[1], ticket[2], ticket[3], ticket[4], ticket[5], ticket[6]));
             }
 
             // Let the user decide what to do
-            switch (input.getMenuOption())
+            var keepRunning = true;
+            while (keepRunning)
             {
-                case "1":
-                    // read file
-                    break;
-                case "2":
-                    string summary = input.getTicketSummary();
-                    string status = input.getTicketStatus();
-                    string priority = input.getTicketPriority();
-                    string submitter = input.getTicketSubmitter();
-                    string assigned = input.getTicketAssigned();
-                    string watching = input.getTicketWatching();
+                switch (_input.GetMenuOption())
+                {
+                    case "1":
+                        Console.WriteLine($"{"ID", -5}{"Summary", -25}{"Status", -15}{"Priority", -15}{"Submitter", -15}{"Assigned", -15}{"Watching", -15}");
+                        foreach (var ticket in _tickets)
+                        {
+                            Console.WriteLine($"{ticket.Id, -5}{ticket.Summary, -25}{ticket.Status, -15}{ticket.Priority, -15}{ticket.Submitter, -15}{ticket.Assigned, -15}{ticket.Watching, -15}");
+                        }
+                        break;
+                    case "2":
+                        string summary = _input.GetTicketSummary();
+                        string status = _input.GetTicketStatus();
+                        string priority = _input.GetTicketPriority();
+                        string submitter = _input.GetTicketSubmitter();
+                        string assigned = _input.GetTicketAssigned();
+                        string watching = _input.GetTicketWatching();
+
+                        Ticket newTicket = new Ticket(null, summary, status, priority, submitter, assigned, watching);
             
-                    tickets.Add(new Ticket(summary, status, priority, submitter, assigned, watching));
-                    break;
+                        _tickets.Add(newTicket);
+                        _fo.AppendTicket(newTicket);
+                        break;
+                    default:
+                        keepRunning = false;
+                        break;
+                }
             }
         }
     }
 
     class UserInput
         {
-            public string getMenuOption()
+            public string GetMenuOption()
             {
-                Console.WriteLine("1) Read tickets from the file.");
+                Console.WriteLine("1) Read current tickets.");
                 Console.WriteLine("2) Add new tickets to the file.");
                 Console.WriteLine("Enter any other key to exit.");
                 return Console.ReadLine();
             }
 
-            public string getTicketSummary()
+            public string GetTicketSummary()
             {
                 Console.WriteLine("Enter the ticket summary:");
                 return Console.ReadLine();
             }
 
-            public string getTicketStatus()
+            public string GetTicketStatus()
             {
                 Console.WriteLine("Enter the ticket status (open/closed):");
                 return Console.ReadLine();
             }
 
-            public string getTicketPriority()
+            public string GetTicketPriority()
             {
                 Console.WriteLine("Enter the ticket priority (low, medium, high):");
                 return Console.ReadLine();
             }
 
-            public string getTicketSubmitter()
+            public string GetTicketSubmitter()
             {
                 Console.WriteLine("Enter the person who submitted the ticket:");
                 return Console.ReadLine();
             }
 
-            public string getTicketAssigned()
+            public string GetTicketAssigned()
             {
                 Console.WriteLine("Enter the person assigned to the ticket:");
                 return Console.ReadLine();
             }
 
-            public string getTicketWatching()
+            public string GetTicketWatching()
             {
                 Console.WriteLine("Enter the person watching the ticket:");
                 Console.WriteLine("(If multiple people are watching, separate names with the | symbol)");
@@ -112,74 +131,48 @@ namespace TicketConsole
         class Ticket
         {
             private static int _ticketCount;
-            private int _id;
-            private string _summary;
-            private string _status;
-            private string _priority;
-            private string _submitter;
-            private string _assigned;
-            private string _watching;
 
-            public Ticket(string summary, string status, string priority, string submitter, string assigned, string watching)
+            public Ticket(string id, string summary, string status, string priority, string submitter, string assigned, string watching)
             {
-                _id = _ticketCount++;
-                _summary = summary;
-                _status = status;
-                _priority = priority;
-                _submitter = submitter;
-                _assigned = assigned;
-                _watching = watching;
+                if (id == null)
+                {
+                    Id = ++_ticketCount;
+                }
+                else
+                {
+                    Id = int.Parse(id);
+                    _ticketCount++;
+                }
+                
+                Summary = summary;
+                Status = status;
+                Priority = priority;
+                Submitter = submitter;
+                Assigned = assigned;
+                Watching = watching;
             }
 
-            public int Id
-            {
-                get => _id;
-                set => _id = value;
-            }
+            public int Id { get; set; }
 
-            public string Summary
-            {
-                get => _summary;
-                set => _summary = value;
-            }
+            public string Summary { get; set; }
 
-            public string Status
-            {
-                get => _status;
-                set => _status = value;
-            }
+            public string Status { get; set; }
 
-            public string Priority
-            {
-                get => _priority;
-                set => _priority = value;
-            }
+            public string Priority { get; set; }
 
-            public string Submitter
-            {
-                get => _submitter;
-                set => _submitter = value;
-            }
+            public string Submitter { get; set; }
 
-            public string Assigned
-            {
-                get => _assigned;
-                set => _assigned = value;
-            }
+            public string Assigned { get; set; }
 
-            public string Watching
-            {
-                get => _watching;
-                set => _watching = value;
-            }
+            public string Watching { get; set; }
 
-            public string GetOutput()
+            public override string ToString()
             {
-                return $"{_summary},{_status},{_priority},{_submitter},{_assigned},{_watching}";
+                return $"{Id},{Summary},{Status},{Priority},{Submitter},{Assigned},{Watching}";
             }
         }
 
-        class FileReader
+        class FileOperations
         {
             private string _file = "Tickets.csv";
             
@@ -196,16 +189,27 @@ namespace TicketConsole
                     while (!sr.EndOfStream)
                     {
                         string line = sr.ReadLine();
-                        if (counter > 1) // Ignore the headers
+                        if (counter > 0) // Ignore the headers
                         {
-                            string[] lineArray = line.Split(',');
-                            tickets.Add(lineArray);
-                            counter++;
+                            if (line != null)
+                            {
+                                string[] lineArray = line.Split(',');
+                                tickets.Add(lineArray);
+                            }
                         }
+                        counter++;
                     }
                     sr.Close();
                 }
                 return tickets;
+            }
+
+            public void AppendTicket(Ticket ticket)
+            {
+                var sw = new StreamWriter(_file, true);
+                
+                sw.WriteLine(ticket.ToString());
+                sw.Close();
             }
         }
     
